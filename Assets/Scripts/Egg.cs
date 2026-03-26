1d3847d71bd9535c7a9aa4af5949a6bcd7bb907c
Justin Shi <justinmshi@icloud.com>
2026-03-26 10:14:14 -0700

    Complete faces on eggs

--- Assets/Scripts/Egg.cs
+++ Assets/Scripts/Egg.cs
@@ -7,10 +7,20 @@ public class Egg : MonoBehaviour
 {
     [SerializeField] private Texture[] _texs;
     [SerializeField] private AudioClip _catchSFX;
+    [SerializeField] private Texture[] _faceTexs;
 
     private void Awake()
     {
-        GetComponent<MeshRenderer>().material.mainTexture = _texs[Random.Range(0, _texs.Length)];
+        Texture tex;
+        if (Random.Range(0, 4) == 0)
+        {
+            tex = _faceTexs[Random.Range(0, _faceTexs.Length)];
+        } else
+        {
+            tex = _texs[Random.Range(0, _texs.Length)];
+        }
+
+        GetComponent<MeshRenderer>().material.mainTexture = tex;
     }
 
     private void Update()
